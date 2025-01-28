0f708b01dcae743e88c5fbc2dd25403b4ec93a00
jamesBoder <james.macean08@gmail.com>
2025-01-28 16:46:49 -0500

    Update App.js
    "Updates"

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const NotFound = () => {
 
 const App = () => {
   return (
-    <Router basename="/first-react-app">
+    <Router basename="/movie_finder_2_react">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Movie Finder</Link>
       </nav>
