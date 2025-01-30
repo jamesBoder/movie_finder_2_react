8246792fda685250661b817e43195b612a3ff548
jamesBoder <james.macean08@gmail.com>
2025-01-30 14:01:46 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes, BrowserRouter } from "react-router-dom";
 import Home from './Home';
 import Movie from './Movie';
 
@@ -11,16 +11,18 @@ const NotFound = () => {
 
 const App = () => {
   return (
-    <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Movie Finder 2</Link>
-      </nav>
-      <Routes>
-        <Route path="/" component={<Home />} />
-        <Route path="/movie/:id" component={<Movie />} />
-        <Route component={NotFound} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">Movie Finder 2</Link>
+        </nav>
+        <Routes>
+          <Route path="/" component={<Home />} />
+          <Route path="/movie/:id" component={<Movie />} />
+          <Route component={NotFound} />
+        </Routes>
+      </Router>
+    </BrowserRouter>
   );
 }
 
