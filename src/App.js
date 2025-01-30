fc6e065f6b1cd10686521383dfc5d5e240cc4974
jamesBoder <james.macean08@gmail.com>
2025-01-30 13:33:34 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from './Home';
 import Movie from './Movie';
 
@@ -15,11 +15,11 @@ const App = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Movie Finder 2</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Movie />} />
-        <Route element={NotFound} />
-      </Routes>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/movie/:id" component={Movie} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
