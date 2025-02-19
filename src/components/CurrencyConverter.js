92e27ff6055687b4357feddc9b3125117e3f2635
jamesBoder <james.macean08@gmail.com>
2025-02-18 22:09:03 -0500

    Update converter

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,26 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './CurrencyConverter.css';
 import CurrencyRow from "./CurrencyRow";
 
-const BASE_URL = 'https://api.apilayer.com/exchangerates_data/latest' 
+const BASE_URL = 'https://api.exchangeratesapi.io/v1/latest?access_key=5a022b803e69a393442d4509eb28cb98'; 
 
 
 
 const CurrencyConverter = () => {
 
+  const[ currencyOptions, setCurrencyOptions] = useState([]);
+  const[ fromCurrency, setFromCurrency] = useState();
+  const[ toCurrency, setToCurrency] = useState();
+  
+
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+      });
 
   }, []);
 
   return (
     <>
       <h1>Currency Converter</h1>
-      <CurrencyRow />
+      <CurrencyRow 
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        
+      />
       <div className="equal-sign">=</div>
-      <CurrencyRow />
+      
+      <CurrencyRow 
+        currencyOptions={currencyOptions} 
+        selectedCurrency={toCurrency}
+
+      />
     </>
 
 
--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props){
+
+  const {currencyOptions, selectedCurrency} = props;
+
   return (
     <div>
       <input type="number" className="input" />
-      <select>
-        <option value="1">USD</option>
+      <select value={selectedCurrency} className="select">
+        {currencyOptions.map((option, index) => (
+          <option key={option} value={option}>{option}</option>
+        ))}
+  
       </select>
     </div>
   )
