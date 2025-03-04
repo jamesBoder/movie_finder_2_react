6666b0a144a8711f57d344d8791483709fc6766c
jamesBoder <james.macean08@gmail.com>
2025-03-04 18:31:04 -0500

    New repos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Welcome from './components/Welcome';
 import LogIn from './components/LogIn';
@@ -7,15 +8,18 @@ import Footer from './components/Footer';
 import CurrencyConverter from './components/CurrencyConverter';
 import './App.css';
 
+
 function App() {
   return (
+    
     <div className="App">
-      <NavBar />
-      <Welcome />
-      <LogIn />
-      <CurrencyConverter />
-      <Footer />
-
+      <Router basename="https://github.com/jamesBoder/exchange_app_react.git">
+        <NavBar />
+        <Welcome />
+        <LogIn />
+        <CurrencyConverter />
+        <Footer />
+      </Router> 
       
     </div>
   );
